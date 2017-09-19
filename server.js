ac7d1a011bcd84c1cba01ba0c255df39531d941f
Matt Kaminski <kaminski1862@gmail.com>
2017-09-18 22:34:22 -0700

    First Time I commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+
+app.use((req, res, next)=>{
+  var now = new Date().toString();
+  var log = `${now}; ${req.method}; ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log +'\n', (err)=>{
+    if(err){
+      console.log('Unable to support log.');
+    }
+  });
+  next();
+});
+app.use((req, res, next)=>{
+    console.log('We couldnt find that site.');
+      res.render('maintenance.hbs',{
+        pageTitle:'Under Maintenance',
+        currentYear: new Date().getFullYear()
+      });
+
+
+});
+// app.get('/', (req, res) =>{
+//
+//   res.send({
+//     name: 'Matt',
+//     likes: [
+//       'Biking',
+//       'Coding'
+//     ]
+//   });
+// });
+
+app.get('/', (req,res) =>{
+  res.render('home.hbs', {
+    pageTitle: 'Home Title',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Hey welcome to home!'
+
+  });
+});
+  //
+
+app.get('/about', (req,res) =>{
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+
+});
+
+app.listen(3000, ()=>{
+  console.log("Server is up on 3000");
+});
